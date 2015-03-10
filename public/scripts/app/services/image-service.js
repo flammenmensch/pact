0f9b9912a8a71ebb49b1044eb1281929ac2e72a8
Flammenmensch <maleventum@gmail.com>
2015-03-11 00:42:09 +0300

    Added watermark

--- public/scripts/app/services/image-service.js
+++ public/scripts/app/services/image-service.js
@@ -2,6 +2,9 @@
 
 module.exports = /*@ngInject*/ function($q, GlFxService) {
 
+    var MAX_WIDTH = 512;
+    var MAX_HEIGHT = 512;
+
     function loadImage(url) {
         return $q(function(resolve) {
             var img = new Image();
@@ -21,35 +24,67 @@ module.exports = /*@ngInject*/ function($q, GlFxService) {
     }
 
     function createImageCanvas(img) {
-        var canvas = createBlankCanvas(img.width, img.height);
+        var width = img.width, height = img.height, ratio = 1.0;
 
-        var ctx = canvas.getContext('2d');
-        ctx.drawImage(img, 0, 0);
+        if (img.width > MAX_WIDTH) {
+            ratio = MAX_WIDTH / img.width;
+            width = MAX_WIDTH;
+            height = img.height * ratio;
+        }
+
+        if (img.height > MAX_HEIGHT) {
+            ratio = MAX_HEIGHT / img.height;
+            width = img.width * ratio;
+            height = MAX_HEIGHT;
+        }
+
+        var canvas = createBlankCanvas(width, height);
+        canvas.getContext('2d').drawImage(img, 0, 0, width, height);
 
         return canvas;
+    }
+
+    function applyBulgeFilter(img, faces) {
+        var canvas = GlFxService.canvas();
+        var texture = canvas.texture(img);
+
+        canvas.draw(texture);
+
+        faces.forEach(function(face) {
+            if (face.hasOwnProperty('eye_left')) {
+                canvas.bulgePinch(face.eye_left.x, face.eye_left.y, 30, 0.5);
+            }
+
+            if (face.hasOwnProperty('eye_right')) {
+                canvas.bulgePinch(face.eye_right.x, face.eye_right.y, 30, 0.5);
+            }
+        });
+
+        canvas.update();
+
+        var dataUrl = canvas.toDataURL('image/png');
+
+        texture.destroy();
+
+        return dataUrl;
     }
 
     function applyTransformation(dataUrl, faces) {
         return loadImage(dataUrl)
             .then(function(img) {
-                var canvas = GlFxService.canvas();
-                var texture = canvas.texture(img);
+                return applyBulgeFilter(img, faces);
+            })
+            .then(loadImage)
+            .then(function(bulgedImg) {
+                return loadImage('images/watermark.png').then(function(watermarkImg) {
+                    var canvas = createBlankCanvas(bulgedImg.width, bulgedImg.height);
+                    var ctx = canvas.getContext('2d');
 
-                canvas.draw(texture);
+                    ctx.drawImage(bulgedImg, 0, 0);
+                    ctx.drawImage(watermarkImg, canvas.width - watermarkImg.width, canvas.height - watermarkImg.height - 20);
 
-                faces.forEach(function(face) {
-                    if (face.hasOwnProperty('eye_left')) {
-                        canvas.bulgePinch(face.eye_left.x, face.eye_left.y, 30, 0.5);
-                    }
-
-                    if (face.hasOwnProperty('eye_right')) {
-                        canvas.bulgePinch(face.eye_right.x, face.eye_right.y, 30, 0.5);
-                    }
+                    return canvas.toDataURL('image/png');
                 });
-
-                canvas.update();
-
-                return canvas.toDataURL('image/png');
             }).catch(function() {
                 return dataUrl;
             });
